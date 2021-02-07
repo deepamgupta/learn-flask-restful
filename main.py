3f48a94c9e5a8faad958dbb6ec9b46fc8828441f
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2021-02-08 00:36:18 +0530

    adding db functionality through sqlalchemy

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes})"
+
+db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument('name', type=str, help='Name of the video is required', required=True)
